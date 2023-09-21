02ac2575da251409f72a016f6b6ef9601826cd53
SUPERKIRBYCHAN <hwg4906@gmail.com>
2023-09-21 12:03:28 +0900

    컨바스 중심의 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -9,20 +9,20 @@ grass = load_image('grass.png')
 clear_canvas_now()
 grass.draw_now(400,30)
 character.draw_now(400,90)
-delay(0.1)
+delay(1)
 
 def run_circle():
     print('CIRCLE')
 
-    r = 200
+    cx, cy, r = 400, 300, 200
 
     for deg in range(0, 360, 5):
-        x = r * math.cos(math.radians(deg))
-        y = r * math.sin(math.radians(deg))
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        delay(0.001)
+        delay(0.1)
         
     pass
 
