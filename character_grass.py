a27d83dab44f67505a147e678ff2351fe0fe6e72
SUPERKIRBYCHAN <hwg4906@gmail.com>
2023-09-21 12:01:27 +0900

    원점에서 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -8,10 +9,21 @@ grass = load_image('grass.png')
 clear_canvas_now()
 grass.draw_now(400,30)
 character.draw_now(400,90)
-delay(1)
+delay(0.1)
 
 def run_circle():
     print('CIRCLE')
+
+    r = 200
+
+    for deg in range(0, 360, 5):
+        x = r * math.cos(math.radians(deg))
+        y = r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.001)
+        
     pass
 
 
